44dc6d2e7e559d50dad0ac8176da64f61239f410
Hyunsoo Kim <gustnqpf3@gmail.com>
2020-09-21 12:34:31 +0900

    Initial Commit

---
+++ SwiftUIList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIList
+//
+//  Created by 김현수 on 2020/09/21.
+//  Copyright © 2020 Hyun Soo Kim. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
